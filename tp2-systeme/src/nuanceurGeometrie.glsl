d3910809883804694bd319f77a78d3d9b851a46e
IssifathSanni <33362689+IssifathSanni@users.noreply.github.com>
2018-02-08 12:20:42 -0500

    IS: nuanceur de geometrie

---
+++ tp2-systeme/src/nuanceurGeometrie.glsl
@@ -0,0 +1,46 @@
+#version 410
+
+layout(triangles) in;
+layout(triangle_strip, max_vertices = 6) out;
+
+in Attribs {
+   vec4 couleur;
+   float clipDistance;
+   float profondeur;
+   float proportionLatitude;
+} AttribsIn[];
+
+out Attribs {
+   vec4 couleur;
+   float profondeur;
+   float proportionLatitude;
+} AttribsOut;
+
+void main()
+{
+   for ( int i = 0 ; i < gl_in.length() ; ++i )
+   {
+      gl_ViewportIndex = 0;
+      gl_ClipDistance[0] = -AttribsIn[i].clipDistance;
+      gl_Position = gl_in[i].gl_Position;
+      AttribsOut.couleur = AttribsIn[i].couleur;
+      AttribsOut.couleurPoleRechauffe = AttribsIn[i].couleurPoleRechauffe;
+      AttribsOut.profondeur = AttribsIn[i].profondeur;
+      AttribsOut.proportionLatitude = AttribsIn[i].proportionLatitude;
+      EmitVertex();
+   }
+   EndPrimitive();
+
+   for ( int i = 0 ; i < gl_in.length() ; ++i )
+   {
+      gl_ViewportIndex = 1;
+      gl_ClipDistance[0] = AttribsIn[i].clipDistance;
+      gl_Position = gl_in[i].gl_Position;
+      AttribsOut.couleur = AttribsIn[i].couleur;
+           AttribsOut.profondeur = AttribsIn[i].profondeur;
+      AttribsOut.proportionLatitude =             		  	 	AttribsIn[i].proportionLatitude;
+      EmitVertex();
+   }
+   EndPrimitive();
+}
+
